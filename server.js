c7077319ffbdd687259ddb20e819e4db3a41f77b
legacyKim <123148778+legacyKim@users.noreply.github.com>
2023-02-21 18:14:52 +0900

    working for mongo db

--- server.js
+++ server.js
@@ -1,31 +1,45 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const path = require('path');
 const app = express();
 
-app.listen(8080, function () {
-  console.log('listening on 8080')
-}); 
+const url = "mongodb+srv://arbam486:93Whelper486@whelper.r0x6ync.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(express.json());
 var cors = require('cors');
 app.use(cors());
 
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(url)
+  .then(res => console.log('mongo db connect'))
+  .catch(err => console.log(err));
+
+app.listen(8080, () => {
+  console.log("Express server started");
+});
+
+// bulid 
+
 app.use(express.static(path.join(__dirname, 'Whelper/build')))
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'Whelper/build/index.html'));
 })
 
-app.get('/WhiteList', function(req, res){
+// bulid 
+
+/*
+
+app.get('/components/Memo', function (req, res) {
   res.json();
 })
 
-app.get('/MemoList', function(req, res){
-  res.json(); 
-})
+*/
 
 // checking for router... but working it is not...
 
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'Whelper/build/index.html'));
 })
